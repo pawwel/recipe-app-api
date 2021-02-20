195593a32e06618c404f6475ad207f701b9bb432
Pawel Drozdz <pawel.drozdz@booksy.com>
2021-02-20 09:55:49 +0100

    manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,13 +16,24 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for user authentication object"""
+    """Serializer for the user authentication object"""
     email = serializers.CharField()
     password = serializers.CharField(
         style={'input_type': 'password'},
-        trim_whitespace=False,
+        trim_whitespace=False
     )
 
     def validate(self, attrs):
@@ -33,10 +44,11 @@ class AuthTokenSerializer(serializers.Serializer):
         user = authenticate(
             request=self.context.get('request'),
             username=email,
-            password=password,
+            password=password
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
+
         attrs['user'] = user
         return attrs
